b072a38c0172111b4e9b2c6b2fe3c743b46a3fd4
KarolisG <kgaivenis@gmail.com>
2024-10-29 13:24:07 +0200

    style: song list component

--- src/store/index.ts
+++ src/store/index.ts
@@ -27,110 +27,120 @@ interface MusicPlayerState {
   updateCurrentTime: () => void
 }
 
-export const useMusicPlayerStore = create<MusicPlayerState>((set, get) => ({
-  trackList: [],
-  currentTrackIndex: null,
-  isPlaying: false,
-  volume: 0.5,
-  favorites: [],
-  audio: null,
-  currentTime: 0,
-  duration: 0,
+export const useMusicPlayerStore = create<MusicPlayerState>((set, get) => {
+  const localFavorites = localStorage.getItem('favorites')
+  const savedFavorites = localFavorites ? JSON.parse(localFavorites) : []
 
-  setTrackList: tracks => set({ trackList: tracks }),
+  return {
+    trackList: [],
+    currentTrackIndex: null,
+    isPlaying: false,
+    volume: 0.5,
+    favorites: savedFavorites,
+    audio: null,
+    currentTime: 0,
+    duration: 0,
 
-  playTrack: index => {
-    const state = get()
-    const track = state.trackList[index]
-    let audio = state.audio
+    setTrackList: tracks => set({ trackList: tracks }),
 
-    // define utlity function to start updating song progress
-    const startUpdatingProgress = () => {
-      const updateProgress = () => {
-        set({ currentTime: audio?.currentTime })
-        if (!audio?.paused) {
-          requestAnimationFrame(updateProgress)
+    playTrack: index => {
+      const state = get()
+      const track = state.trackList[index]
+      let audio = state.audio
+
+      // define utlity function to start updating song progress
+      const startUpdatingProgress = () => {
+        const updateProgress = () => {
+          set({ currentTime: audio?.currentTime })
+          if (!audio?.paused) {
+            requestAnimationFrame(updateProgress)
+          }
         }
+        requestAnimationFrame(updateProgress)
       }
-      requestAnimationFrame(updateProgress)
-    }
 
-    // Create a new audio instance if none exists
-    if (!audio) {
-      audio = new Audio(track.src)
-      audio.volume = state.volume
+      // Create a new audio instance if none exists
+      if (!audio) {
+        audio = new Audio(track.src)
+        audio.volume = state.volume
 
-      // Set states, start song and track song progress when metadata is loaded
-      audio.onloadedmetadata = () => {
-        set({ duration: audio?.duration, audio, currentTrackIndex: index, isPlaying: true })
-        audio?.play()
-        startUpdatingProgress()
+        // Set states, start song and track song progress when metadata is loaded
+        audio.onloadedmetadata = () => {
+          set({ duration: audio?.duration, audio, currentTrackIndex: index, isPlaying: true })
+          audio?.play()
+          startUpdatingProgress()
+        }
+        return
       }
-      return
-    }
 
-    // Handle track switch
-    if (state.currentTrackIndex !== index) {
+      // Handle track switch
+      if (state.currentTrackIndex !== index) {
+        if (state.isPlaying) {
+          audio.pause()
+        }
+        audio.src = track.src
+
+        audio.onloadedmetadata = () => {
+          set({ duration: audio?.duration, audio, currentTrackIndex: index, isPlaying: true })
+          audio.play()
+          startUpdatingProgress()
+        }
+        return
+      }
+
+      // Toggle play/pause
       if (state.isPlaying) {
         audio.pause()
-      }
-      audio.src = track.src
-
-      audio.onloadedmetadata = () => {
-        set({ duration: audio?.duration, audio, currentTrackIndex: index, isPlaying: true })
+        set({ isPlaying: false })
+      } else {
         audio.play()
+        set({ isPlaying: true })
         startUpdatingProgress()
       }
-      return
-    }
+    },
 
-    // Toggle play/pause
-    if (state.isPlaying) {
-      audio.pause()
-      set({ isPlaying: false })
-    } else {
-      audio.play()
-      set({ isPlaying: true })
-      startUpdatingProgress()
-    }
-  },
+    updateCurrentTime: () => {
+      const { audio } = get()
+      if (audio) {
+        set({
+          currentTime: audio.currentTime,
+          duration: audio.duration,
+        })
+      }
+    },
 
-  updateCurrentTime: () => {
-    const { audio } = get()
-    if (audio) {
-      set({
-        currentTime: audio.currentTime,
-        duration: audio.duration,
+    // modulo operator here ensures that playlist will behave in circular way. without it there would be out-of-bounds indices (for example -1 or more than the whole playlist length)
+    nextTrack: () => {
+      const { currentTrackIndex, trackList, playTrack } = get()
+      const nextIndex = (currentTrackIndex + 1) % trackList.length
+      playTrack(nextIndex)
+    },
+
+    prevTrack: () => {
+      const { currentTrackIndex, trackList, playTrack } = get()
+      const prevIndex = (currentTrackIndex - 1 + trackList.length) % trackList.length
+      playTrack(prevIndex)
+    },
+
+    setVolume: volume => {
+      const { audio } = get()
+      if (audio) {
+        audio.volume = volume
+      }
+      set({ volume })
+    },
+
+    // using the spread operator to manage the favorites array is ok because it is only a flat array of primitive values. Otherwise, Immer could be a solution to use
+    toggleFavorite: id => {
+      set(state => {
+        const updatedFavorites = state.favorites.includes(id)
+          ? state.favorites.filter(favId => favId !== id)
+          : [...state.favorites, id]
+
+        localStorage.setItem('favorites', JSON.stringify(updatedFavorites))
+
+        return { favorites: updatedFavorites }
       })
-    }
-  },
-
-  // modulo operator here ensures that playlist will behave in circular way. without it there would be out-of-bounds indices (for example -1 or more than the whole playlist length)
-  nextTrack: () => {
-    const { currentTrackIndex, trackList, playTrack } = get()
-    const nextIndex = (currentTrackIndex + 1) % trackList.length
-    playTrack(nextIndex)
-  },
-
-  prevTrack: () => {
-    const { currentTrackIndex, trackList, playTrack } = get()
-    const prevIndex = (currentTrackIndex - 1 + trackList.length) % trackList.length
-    playTrack(prevIndex)
-  },
-
-  setVolume: volume => {
-    const { audio } = get()
-    if (audio) {
-      audio.volume = volume
-    }
-    set({ volume })
-  },
-
-  // using the spread operator to manage the favorites array is ok because it is only a flat array of primitive values. Otherwise, Immer could be a solution to use
-  toggleFavorite: id =>
-    set(state => ({
-      favorites: state.favorites.includes(id)
-        ? state.favorites.filter(favId => favId !== id)
-        : [...state.favorites, id],
-    })),
-}))
+    },
+  }
+})
